cdfaadf74daf7be7d8b64beccc1c515330be5182
integral05 <tejastathe4337@gmail.com>
2025-10-04 14:24:45 +0530

    working on themes of application

--- word_appendix_manager/src/gui/dialogs/__init__.py
+++ word_appendix_manager/src/gui/dialogs/__init__.py
@@ -1,1 +1,18 @@
-"""GUI Dialogs Package - will be populated later""" 
+"""
+GUI Dialogs Package
+"""
+
+from gui.dialogs.settings_dialog import SettingsDialog
+
+# Try to import optional dialogs
+try:
+    from gui.dialogs.appendix_edit_dialog import AppendixEditDialog
+except ImportError:
+    AppendixEditDialog = None
+
+try:
+    from gui.dialogs.preview_dialog import PreviewDialog
+except ImportError:
+    PreviewDialog = None
+
+__all__ = ['SettingsDialog', 'AppendixEditDialog', 'PreviewDialog']
